0a92c76138bc58bcd677c65fc7cd9155d4adf2aa
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-28 20:42:42 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(words)
+  array = words.split(/ /)
+  reverse_array = []
+    array.collect do |words|
+    reverse_array.push(words.reverse)
+  end
+  reverse_array.join(" ")
+end
+
+#each collect
